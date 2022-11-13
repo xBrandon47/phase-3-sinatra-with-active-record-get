56b627cb48325c3accb6bf2488d27024d8129f09
Brandon Moreno <brandonm99@me.com>
2022-11-12 21:13:22 -0700

    commit tree 515ce0c40cdbaea1e29e4d6d07a62c0bbe1667fa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
-  end
+  set :default_content_type, 'application/json'
 
+  gget '/games/:id' do
+    game = Game.find(params[:id])
+
+    # include associated reviews in the JSON response
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
+  end
 end
